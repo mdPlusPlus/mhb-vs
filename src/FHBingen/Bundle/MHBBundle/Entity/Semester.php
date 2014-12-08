e99b17b1671d0bb00db027a251750bc391f92b2b
Mischa <m.nehrbass@fh-bingen.de>
2014-12-08 13:34:57 +0100

    Semester Ref regelsemester weg

--- src/FHBingen/Bundle/MHBBundle/Entity/Semester.php
+++ src/FHBingen/Bundle/MHBBundle/Entity/Semester.php
@@ -190,12 +190,12 @@ class Semester
 
 
     /*Studienplan*/
-
-    /*Regelsemester*/
+    /*Startsemester*/
     /**
-     * @ORM\OneToMany(targetEntity="Studienplan", mappedBy="start_sem", cascade={"all"})
+     * @ORM\OneToMany(targetEntity="Studienplan", mappedBy="reg_sem", cascade={"all"})
      * */
-    protected $startsem;
+    protected $regelsem;
+
 
     /**
      * Add regelsem
