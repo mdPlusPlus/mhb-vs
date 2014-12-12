040c897a7aecc1c6c5093cd1075e8fcb45bf1142
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2014-12-12 22:28:29 +0100

    extends Controller

--- src/FHBingen/Bundle/MHBBundle/Controller/RestrictedController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/RestrictedController.php
@@ -15,10 +15,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\SecurityContextInterface;
 
 
-class RestrictedController
+class RestrictedController extends Controller
 {
 
     /**
