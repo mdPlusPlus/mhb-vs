2ae7882948737330f9f5e4c70964f9f168a098cc
Christian <christian-wittig@outlook.com>
2014-12-13 00:10:08 +0100

    Template verkettung Login

--- src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
@@ -27,8 +27,40 @@ class DefaultController extends Controller
      */
     public function SglMainAction()
     {
-        $userID='a';
-        return array('name' => $userID,'pageTitle' => 'STARTSEITE');
+        //Filter auf Status
+        //Filter auf Modul
+        //Filter auf Versionsnummer
+        //Abfangen falls keine Module vorhanden sind
+        //Email anpassen
+
+//        $user = $this->get('security.context')->getToken()->getUser();
+//        $user_mail = $user->getUsername(); $user_mail
+        $em = $this->getDoctrine()->getManager();
+        $dozent = $em->getRepository('FHBingenMHBBundle:Dozent')->findOneBy(array('Email'=> 'schmidt@fh-bingen.de'));
+        $modulverantwortung = $em->getRepository('FHBingenMHBBundle:Veranstaltung')->findBy(array('beauftragter' => $dozent->getDozentenID()));
+
+        $mLehrende=array();
+        foreach($modulverantwortung as $m)
+        {
+            $name=array();
+            $tmp = $em->getRepository('FHBingenMHBBundle:Lehrende')->findBy(array('module' => $m->getModulID()));
+
+            foreach($tmp as $lehrend)
+            {
+                $name[]=(string)$lehrend->getLehrender();
+            }
+            $mLehrende[]=$name;
+        }
+
+        $entries= $em->getRepository('FHBingenMHBBundle:Lehrende')->findBy(array('lehrender'=> $dozent->getDozentenID()));
+
+        $modullehrend =array();
+        foreach($entries as $modul)
+        {
+            $modullehrend[]=$em->getRepository('FHBingenMHBBundle:Veranstaltung')->findOneBy(array('Modul_ID'=> $modul->getModule()));
+        }
+
+        return array('modulverantwortung' => $modulverantwortung, 'modullehrend' => $modullehrend, 'mLehrende' => $mLehrende,'pageTitle' => 'STARTSEITE');
     }
 
     /**
