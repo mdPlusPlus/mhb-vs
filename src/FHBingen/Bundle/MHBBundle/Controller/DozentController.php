9f512d4791488cf2befc491e77afeaccf85122f9
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2014-12-17 03:02:09 +0100

    leere Controller für späteres Splitting erstellt

---
+++ src/FHBingen/Bundle/MHBBundle/Controller/DozentController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdPlusPlus
+ * Date: 17.12.2014
+ * Time: 03:00
+ */
+
+namespace FHBingen\Bundle\MHBBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DozentController extends Controller{
+/*
+ * TODO
+ * Alle Dozenten-Funktionen hier rein
+ */
+}
---
+++ src/FHBingen/Bundle/MHBBundle/Controller/SglController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdPlusPlus
+ * Date: 17.12.2014
+ * Time: 03:00
+ */
+
+namespace FHBingen\Bundle\MHBBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SglController extends Controller{
+    /*
+     * TODO
+     * Alle SGL-Funktionen hier rein
+     */
+}
