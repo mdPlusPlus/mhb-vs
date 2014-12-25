26259e4e6ed5537fda8a6bb8c013d1b8878a6b35
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2014-12-25 13:24:19 +0100

    aufgeräumt

--- src/FHBingen/Bundle/MHBBundle/Controller/LoginController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/LoginController.php
@@ -29,9 +29,7 @@ class LoginController extends Controller
 
         // get the login error if there is one
         if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContextInterface::AUTHENTICATION_ERROR
-            );
+            $error = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
         } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
             $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
             $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
