b35da37dff237ded6b72af6ad6aed3a017044867
Mischa <m.nehrbass@fh-bingen.de>
2014-12-28 14:54:34 +0100

    PlanungType

--- src/FHBingen/Bundle/MHBBundle/Form/PlanungType.php
+++ src/FHBingen/Bundle/MHBBundle/Form/PlanungType.php
@@ -27,7 +27,7 @@ class PlanungType extends AbstractType
             ->add('name', 'text', array('label' => 'Modulname (deutsch): ', 'required' => true))
             ->add('nameEN', 'text', array('label' => 'Modulname (englisch): ', 'required' => false))
             ->add('haeufigkeit', 'choice', array('label' => 'Häufigkeit des Angebots: ', 'required' => false, 'choices' => $frequency))
-//             ->add('dauer', 'text',  array('label' => 'Dauer: ', 'required' => false))
+             ->add('dauer', 'text',  array('label' => 'Dauer: ', 'required' => false))
 //            ->add('lehrveranstaltungen', 'choice',  array('label' => 'Lehrveranstaltungen: ','choices'
 //            => array('Vorlesung'   => 'Vorlesung',
 //                    'Übung' => 'Übung',
@@ -37,12 +37,12 @@ class PlanungType extends AbstractType
 //                    'Selbststudium und Konsultationen' => 'Selbststudium und Konsultationen'),
 //                'multiple'  => true,
 //                'expanded' => true))
-//            ->add('kontaktzeitVL', 'integer', array('label' => 'Kontaktzeit Vorlesung: ', 'required' => false))
-//            ->add('kontaktzeitSonstige', 'integer', array('label' => 'Kontaktzeit sonstige: ', 'required' => false))
-//            ->add('selbststudium', 'integer', array('label' => 'Selbststudium: ', 'required' => false))
-//            ->add('gruppengroesse', 'integer', array('label' => 'Gruppengröße: ', 'required' => false))
-//            ->add('lernergebnisse', 'textarea', array('label' => 'Lernergebisse: ', 'required' => false))
-//            ->add('inhalte', 'textarea', array('label' => 'Lehrinhalte: ', 'required' => false))
+            ->add('kontaktzeitVL', 'integer', array('label' => 'Kontaktzeit Vorlesung: ', 'required' => false))
+            ->add('kontaktzeitSonstige', 'integer', array('label' => 'Kontaktzeit sonstige: ', 'required' => false))
+            ->add('selbststudium', 'integer', array('label' => 'Selbststudium: ', 'required' => false))
+            ->add('gruppengroesse', 'integer', array('label' => 'Gruppengröße: ', 'required' => false))
+            ->add('lernergebnisse', 'textarea', array('label' => 'Lernergebisse: ', 'required' => false))
+            ->add('inhalte', 'textarea', array('label' => 'Lehrinhalte: ', 'required' => false))
 //            ->add('pruefungsformen', 'choice', array('label' => 'Prüfungsform: ','choices'
 //            => array('Schriftliche Klausur'   => 'Schriftliche Klausur',
 //                     'Mündliche Prüfung' => 'Mündliche Prüfung',
@@ -55,15 +55,15 @@ class PlanungType extends AbstractType
 //                     'Kolloquium'   => 'Kolloquium'),
 //                     'multiple'  => true,
 //                     'expanded' => true))
-//            ->add('sprache', 'choice', array('label' => 'Sprache: ', 'required' => false, 'choices' => $lang))
-//            ->add('literatur', 'textarea', array('label' => 'Literaturverweise: ', 'required' => false))
-//            ->add('leistungspunkte', 'choice', array('label' => 'Leistungspunkte: ', 'required' => false, 'choices' => $lp))
+            ->add('sprache', 'choice', array('label' => 'Sprache: ', 'required' => false, 'choices' => $lang))
+            ->add('literatur', 'textarea', array('label' => 'Literaturverweise: ', 'required' => false))
+            ->add('leistungspunkte', 'choice', array('label' => 'Leistungspunkte: ', 'required' => false, 'choices' => $lp))
 //            ->add('voraussetzungLP', 'choice', array('label' => 'Voraussetzung für Leistungspunkte: ','choices'
 //            => array('Prüfungsleistung'   => 'Prüfungsleistung',
 //                     'Studienleistung' => 'Studienleistung'),
 //                     'multiple'  => true,
 //                     'expanded' => true))
-//             ->add('voraussetzungInh', 'textarea', array('label' => 'Voraussetzung inhaltlich: ', 'required' => false))
+             ->add('voraussetzungInh', 'textarea', array('label' => 'Voraussetzung inhaltlich: ', 'required' => false))
 
             ->add('reset', 'reset')
             ->add('submit', 'submit');
