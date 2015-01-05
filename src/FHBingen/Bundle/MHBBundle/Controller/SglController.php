3ac4eb562d21fe5dbb3736a04d96330830574843
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-01-05 13:45:44 +0100

    fixing missing import (JsonEncoder)

--- src/FHBingen/Bundle/MHBBundle/Controller/SglController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/SglController.php
@@ -8,13 +8,13 @@
 
 namespace FHBingen\Bundle\MHBBundle\Controller;
 
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FHBingen\Bundle\MHBBundle\Entity;
 use FHBingen\Bundle\MHBBundle\Form;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class SglController extends Controller
 {
