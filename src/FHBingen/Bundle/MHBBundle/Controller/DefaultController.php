343be2830a17ece96d675af3f46c7db05d651fbe
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-01-17 22:23:42 +0100

    InsertController gelöscht

--- src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
@@ -26,4 +26,31 @@ class DefaultController extends Controller
         //'login' ist hier route alias von /login
         return $this->redirect($this->generateUrl('login'));
     }
+
+    //von InsertController
+    public function semesterCreate()
+    {
+        //legt die Semester-Objekte an und gibt sie als Array zurück
+        //TODO: In Oberfäche integieren ?
+        $semester0 = new Semester();
+        $semester0->setSemester('WS14');
+
+        $semester1 = new Semester();
+        $semester1->setSemester('SS15');
+
+        $semester2 = new Semester();
+        $semester2->setSemester('WS15');
+
+        $semester3 = new Semester();
+        $semester3->setSemester('SS16');
+
+        $semesterArr = array(
+            $semester0,
+            $semester1,
+            $semester2,
+            $semester3
+        );
+
+        return $semesterArr;
+    }
 }
