e7ed01e8512b932572fdc988b34022a99b86b80d
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-01-17 01:44:38 +0100

    button fix

--- src/FHBingen/Bundle/MHBBundle/Form/ModulhandbuchType.php
+++ src/FHBingen/Bundle/MHBBundle/Form/ModulhandbuchType.php
@@ -18,9 +18,7 @@ class ModulhandbuchType extends AbstractType
         //TODO wird das ModulhandbuchTyp überhaupt benötigt?
         $builder
             ->add('beschreibung', 'text', array('label' => 'Beschreibung: ', 'required' => true))
-            ->add('gueltigAb', 'entity', array('label' => 'Gueltig ab: ', 'required' => true, 'class' => 'FHBingenMHBBundle:Semester'))
-            ->add('reset', 'reset')
-            ->add('submit', 'submit');
+            ->add('gueltigAb', 'entity', array('label' => 'Gueltig ab: ', 'required' => true, 'class' => 'FHBingenMHBBundle:Semester'));
     }
 
     public function getName()
