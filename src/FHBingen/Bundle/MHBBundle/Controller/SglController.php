7f5efb189065807c8310a6bee373d087a862c445
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-01-24 13:53:22 +0100

    toc wieder aktiviert

--- src/FHBingen/Bundle/MHBBundle/Controller/SglController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/SglController.php
@@ -306,9 +306,9 @@ class SglController extends Controller
             'footer-right' => '[page]/[toPage]',
             'title' => $mhb->getBeschreibung(), //TODO: Ändern auf $studiengang + $versionsnummer?
             'disable-javascript' => true,
-            'no-outline' => true,
+            //'no-outline' => true,
             //'cover' => 'cover.html',
-            //'toc' => true,
+            'toc' => true,
             //'xsl-style-sheet' => 'toc.xsl'
             //'dump-outline' => 'outline.xml',
         ));
