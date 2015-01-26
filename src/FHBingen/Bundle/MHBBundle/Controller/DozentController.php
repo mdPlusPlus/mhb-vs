0a2aaa8ae6b2daf486f2f7e15a6e520c2cb3503c
Christian <christian-wittig@outlook.com>
2015-01-26 12:20:53 +0100

    Kommentare

--- src/FHBingen/Bundle/MHBBundle/Controller/DozentController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/DozentController.php
@@ -33,7 +33,6 @@ class DozentController extends Controller
          * 1. Filter auf Status 'Freigegeben'
          * 2. Sortiertung
          * (3. Wenn mehrmals als Lehrender eingetragen, taucht es auch mehrmals hier auf -> fixen beim Freigeben!)
-         * sdfgnsjgnlskdgsnlkg
          */
 
         $user = $this->get('security.context')->getToken()->getUser();
