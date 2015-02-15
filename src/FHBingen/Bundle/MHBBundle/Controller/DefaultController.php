01df15e40759176cb2e7739eb3c0f30ca567537c
Christian <christian-wittig@outlook.com>
2015-02-15 19:02:45 +0100

    Benutzerdoku link

--- src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
@@ -38,7 +38,7 @@ class DefaultController extends Controller
      */
     public function dokuDownloadAction()
     {
-        $downloadPath = 'Dokumentation.pdf';
+        $downloadPath = 'Benutzerdokumentation.pdf';
 
         //return new BinaryFileResponse($pdfPath);
         return new BinaryFileResponse(
