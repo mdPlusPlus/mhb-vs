2958b7bc108f1e94ddb226b01222bafab76e679e
Christian <christian-wittig@outlook.com>
2015-02-15 19:00:09 +0100

    Benutzerdoku link

--- src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
+++ src/FHBingen/Bundle/MHBBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 
