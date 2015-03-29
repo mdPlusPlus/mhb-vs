a66e5813e9aee93418679946f6452377d5cf7ce8
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-03-29 16:42:54 +0200

    deleted everything for repo cleaning

--- bin/wkhtmltopdf-amd64.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64
-php "%BIN_TARGET%" %*
--- src/FHBingen/Bundle/MHBBundle/FHBingenMHBBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FHBingen\Bundle\MHBBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FHBingenMHBBundle extends Bundle
-{
-}
