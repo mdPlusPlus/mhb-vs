ec53490ecc0cd2457a4d8a5e59d2a2ff66f893ce
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-04-17 13:53:56 +0200

    ALTER TABLE Kernfach DROP PRIMARY KEY; ALTER TABLE Kernfach DROP Kernfach_ID; ALTER TABLE Kernfach ADD PRIMARY KEY (modul, vertiefung);

--- src/FHBingen/Bundle/MHBBundle/Entity/Kernfach.php
+++ src/FHBingen/Bundle/MHBBundle/Entity/Kernfach.php
@@ -19,48 +19,39 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Table(name="Kernfach")
  * @ORM\HasLifecycleCallbacks
  */
-
 class Kernfach
 {
 
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $Kernfach_ID;
-
-    /*Abhaengigkeiten*/
-
-    /*Kernfach*/
+//    /**
+//     * @ORM\Id
+//     * @ORM\Column(type="integer")
+//     * @ORM\GeneratedValue(strategy="AUTO")
+//     */
+//    protected $Kernfach_ID;
 
     /**
+     * @ORM\Id()
      * @ORM\ManyToOne(targetEntity="Veranstaltung", inversedBy="kernfach")
      * @ORM\JoinColumn(name="modul", referencedColumnName="Modul_ID", nullable=false)
      * */
     protected $veranstaltung;
 
-
-
-    /*Vertiefung*/
-
     /**
+     * @ORM\Id()
      * @ORM\ManyToOne(targetEntity="Vertiefung", inversedBy="vertiefung")
      * @ORM\JoinColumn(name="vertiefung", referencedColumnName="Vertiefung_ID", nullable=false)
      * */
     protected $vertiefung;
 
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getKernfach_ID()
-    {
-        return $this->Kernfach_ID;
-    }
+//    /**
+//     * Get id
+//     *
+//     * @return integer
+//     */
+//    public function getKernfach_ID()
+//    {
+//        return $this->Kernfach_ID;
+//    }
 
     /**
      * Set modul
