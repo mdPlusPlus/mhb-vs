381411ff385b356f1c4411d39acf2ff258a98ffc
Mischa <m.nehrbass@fh-bingen.de>
2015-05-13 11:48:10 +0200

    DB Anpassung (PORD)

--- src/FHBingen/Bundle/MHBBundle/Tests/Controller/DozentControllerTest.php
+++ src/FHBingen/Bundle/MHBBundle/Tests/Controller/DozentControllerTest.php
@@ -12,12 +12,27 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DozentControllerTest extends WebTestCase
 {
-    public function whatTheFuckTest()
+    public function alleModuleTest()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/restricted/dozent/eigeneModule');
 
-        $form = $crawler->selectButton('speichern')->form();
+        $this->assertGreaterThanOrEqual(
+            0, $crawler->$beauftragteModule->count()
+        );
+
+        $this->assertGreaterThanOrEqual(
+            1, $crawler->$mLehrende->count()
+        );
+
+        $this->assertGreaterThanOrEqual(
+          1, $crawler->$stgZuModul->count()
+        );
+
+        $this->assertLessThanOrEqual(
+          4, $crawler->$stgZuModul->count()
+        );
     }
+
 }
