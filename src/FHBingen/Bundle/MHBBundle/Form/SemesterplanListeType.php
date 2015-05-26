031c6ada9a430eca3837fead97c55a760d6b9bc6
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2015-05-26 17:20:18 +0200

    test

---
+++ src/FHBingen/Bundle/MHBBundle/Form/SemesterplanListeType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdPlusPlus
+ * Date: 26.05.2015
+ * Time: 17:20
+ */
+
+namespace FHBingen\Bundle\MHBBundle\Form;
+
+
+class SemesterplanListeType {
+
+}
